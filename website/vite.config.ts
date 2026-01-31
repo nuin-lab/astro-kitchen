eba69a785911cb999ce4422f198a291933023132
nuin <62213079+nui-n@users.noreply.github.com>
2026-01-31 23:39:24 +0900

    =*: 404ページのビルド設定を追加

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,5 +1,10 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig, mergeConfig } from 'vite';
 import baseViteConfig from '@astro-kitchen/vite-config';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default mergeConfig(
   baseViteConfig,
@@ -8,6 +13,12 @@ export default mergeConfig(
     build: {
       outDir: '../dist',
       emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          404: resolve(__dirname, '404.html'),
+        },
+      },
     },
 
     server: {
