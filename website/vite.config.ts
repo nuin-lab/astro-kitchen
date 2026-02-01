1abce1e776a6bc08dc767345d70d833991e411dd
nui-n <62213079+nui-n@users.noreply.github.com>
2026-02-01 16:26:22 +0900

    +: 404ページの追加 (#9)
    * =*: アプリケーションタイプの変更
    
    * =+: @types/nodeの追加
    
    * =*: 404ページのビルド設定を追加
    
    * +: PCデザインの完成
    
    * +: モバイルデザインの完成

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,12 +1,24 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig, mergeConfig } from 'vite';
 import baseViteConfig from '@astro-kitchen/vite-config';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default mergeConfig(
   baseViteConfig,
   defineConfig({
+    appType: 'mpa',
     build: {
       outDir: '../dist',
       emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          404: resolve(__dirname, '404.html'),
+        },
+      },
     },
 
     server: {
